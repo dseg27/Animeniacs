9ee61dd57216051399c52154b8396e6427069856
Cole Herman <cole.herman370@outlook.com>
2022-02-23 22:45:33 -0600

    Adding the start to cleaning chart data

---
+++ chart_data_cleaning.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+def genreList(genre_string):
+        genre_string = genre_string.replace('[','')
+        genre_string = genre_string.replace(']','')
+        genre_string = genre_string.replace("'",'')
+        genre_list = list(genre_string.split(', '))
+
+        return genre_list
+
+def genreCount(anime_df, netflix_df, amazon_df, hulu_df, disney_df):
+    genre_df = anime_df[['uid','genre']]
+    # genre_df_head = genre_df.head(5)
+    unique_list = []
+
+    for index, row in genre_df.iterrows():
+        genre_list = genreList(row['genre'])
+
+        for x in genre_list:
+            if x not in unique_list:
+                unique_list.append(x)
+                
+        for x in genre_list:
+            if x in unique_list:
+                genre_df.loc[index,x] = 'T'
+
+    genre_count_series = genre_df.count()
+    genre_count_df = pd.DataFrame({'Genre': genre_count_series.index, 'Count': genre_count_series.values})
+    genre_count_df = genre_count_df.drop(labels=[0])
+    genre_count_df = genre_count_df.drop(labels=[1])
+    genre_count_df = genre_count_df[genre_count_df['Genre'] != '']
+    genre_count_df = genre_count_df.sort_values('Count',ascending=False)
+
+    genre_count_df.to_csv('data/genre_count_list.csv')
