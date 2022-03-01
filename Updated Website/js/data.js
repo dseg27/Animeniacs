b937ecdf95aaa510117933dc4e3328fd19dada03
dseg27 <90593897+dseg27@users.noreply.github.com>
2022-02-28 17:49:04 -0700

    Updating website- added searchable dropdown menu.

---
+++ Updated Website/js/data.js
@@ -0,0 +1,59 @@
+// CONVERT JSON FILES TO READABLE DATA 
+
+// // PRINT ANIME TITLES TO CONSOLE
+// var anime_data = fetch("data.json")
+//         .then(response => response.json())
+//         .then(anime_data => {
+//             for (i =0; i < anime_data.length; i++){
+//                 console.log(data[i].title);
+//             }
+//         })
+
+// CREATES A LIST 
+var anime_data = fetch("data.json")
+            .then(response => response.json())
+            .then(anime_data =>{
+                for (i=0; i<anime_data.length; i++){
+                    anime_data[i] = anime_data[i].title;
+                }
+            console.log(anime_data[0])
+            })
+
+
+// // PRINT LIVE ACTION TITLE NAMES TO CONSOLE
+// var live_data = fetch("live_data.json")
+//         .then(response => response.json())
+//         .then(live_data =>{
+//             for (i=0; i<live_data.length; i++){
+//                 console.log(live_data[i].title);
+//             }
+//         })
+
+
+// CREATES A LIST 
+var live_data = fetch("live_data.json")
+            .then(response => response.json())
+            .then(live_data =>{
+                for (i=0; i<live_data.length; i++){
+                    live_data[i] = live_data[i].title;
+                }
+            console.log(live_data[0])
+            })
+  
+
+
+// CREATE SEARCHABLE DROP DOWN BAR 
+// https://makitweb.com/make-a-dropdown-with-search-box-using-jquery/
+
+$(document).ready(function(){
+    // Initialize select2
+    $(".s2").select2();
+
+    // Read selected option
+    $('#but_read').click(function(){
+        var title = $('#selUser option:selected').text();
+        $('#result').html("Selected Title: " + title);
+      });
+  });
+
+
